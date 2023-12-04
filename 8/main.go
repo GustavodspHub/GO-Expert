ed376f742ce73dcd4a51d7fb7770b7f0e28c3064
Gustavo Pereira <gustavopereira0979@gmail.com>
2023-12-04 19:38:38 -0300

    feat: functions

---
+++ 8/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	value, error := sum(5, 50)
+	if error != nil {
+		fmt.Println(error)
+	}
+	fmt.Println(value)
+}
+
+func sum(numberOne, numberTwo int) (int, error) {
+	if numberOne+numberTwo >= 50 {
+		return 0, errors.New("A soma ultrapassou de 50")
+	}
+
+	return numberOne + numberTwo, nil
+}
