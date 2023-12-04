3ae595c779a3471e5fd63b7632d17d74fe6030aa
Gustavo Pereira <gustavopereira0979@gmail.com>
2023-12-04 18:01:20 -0300

    feat: maps

---
+++ 7/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	salaries := map[string]int{"Gustavo": 10000, "Millena": 50000}
+	fmt.Println(salaries["Gustavo"])
+	delete(salaries, "Gustavo")
+	salaries["Gustavo Teste"] = 100000
+	fmt.Println(salaries)
+
+	newSalary := make(map[string]int)
+	newSalary["Richard Thompson"] = 100
+	newSalaryTest := map[string]int{}
+	newSalaryTest["Roberto"] = 200
+
+	for name, salary := range salaries {
+		fmt.Printf("O %s tem um salário de %d\n", name, salary)
+	}
+
+	for _, salary := range salaries {
+		fmt.Printf("Você recebera um salário de %d\n", salary)
+	}
+
+}
