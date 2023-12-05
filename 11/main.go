5044a1b0b494aa49b8f8810018ed0e2a4a98fcf5
Gustavo Pereira <gustavopereira0979@gmail.com>
2023-12-04 21:57:59 -0300

    feat: complements the class about structs

--- 11/main.go
+++ 11/main.go
@@ -26,4 +26,16 @@ func main() {
 	}
 
 	fmt.Printf("My client is %s, your age is %d, and you active status is %t\n.", newClientTwo.name, newClientTwo.age, newClientTwo.active)
+
+	newClient.name = "Roberto"
+	newClient.age = 30
+	newClient.active = false
+
+	fmt.Printf("My client is %s, your age is %d, and you active status is %t.\n", newClient.name, newClient.age, newClient.active)
+
+	newClientTwo.name = "Gisele"
+	newClientTwo.age = 49
+	newClientTwo.active = true
+
+	fmt.Printf("My client is %s, your age is %d, and you active status is %t\n.", newClientTwo.name, newClientTwo.age, newClientTwo.active)
 }
