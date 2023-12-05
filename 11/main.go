3df8d0bb500b96aa41e08991ad6865f717e60475
Gustavo Pereira <gustavopereira0979@gmail.com>
2023-12-04 21:53:55 -0300

    feat: structs

---
+++ 11/main.go
@@ -0,0 +1,29 @@
+/*Structs*/
+
+package main
+
+import "fmt"
+
+func main() {
+	type Client struct {
+		name   string
+		age    int
+		active bool
+	}
+
+	newClient := Client{
+		name:   "Gustavo",
+		age:    20,
+		active: true,
+	}
+
+	fmt.Printf("My client is %s, your age is %d, and you active status is %t.\n", newClient.name, newClient.age, newClient.active)
+
+	newClientTwo := Client{
+		name:   "Millena",
+		age:    19,
+		active: false,
+	}
+
+	fmt.Printf("My client is %s, your age is %d, and you active status is %t\n.", newClientTwo.name, newClientTwo.age, newClientTwo.active)
+}
